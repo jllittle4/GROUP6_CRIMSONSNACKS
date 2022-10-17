fcfc45cc8d78e0f0e472d545aa5840b9b4abc301
riddlemeS4m <swriddle@crimson.ua.edu>
2022-10-17 17:16:01 -0500

    committing admin branch -sam

--- api/Models/Request.cs
+++ api/Models/Request.cs
@@ -2,9 +2,9 @@ namespace api.Models
 {
     public class Request
     {
-        public string UserName {get; set;}
-        public DateTime Date {get; set;}
-        public TimeOnly Clock
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+        public TimeOnly Clock { get; set; }
 
     }
 }
